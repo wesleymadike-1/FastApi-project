be5f72d72f5df72ed1b8aa197bdc38ecb7985e42
WESLEY_MDK <232195016+wesleymadike-1@users.noreply.github.com>
2026-01-12 08:16:04 +0200

    Create CRUD paths for user, implement password hashing, and update User model: set id to random and unique.

--- app/db_models.py
+++ app/db_models.py
@@ -1,13 +1,17 @@
 from sqlalchemy import Column, Integer, String, Boolean, Text, TIMESTAMP, text
 from .database import Base
 from datetime import datetime, timezone
+import random
 
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    def generate_user_id():
+        return random.randint(100, 99999)
+
+    id = Column(Integer, primary_key=True, unique=True, index=True, default=generate_user_id)
     username = Column(String(50), unique=True, nullable=False, index=True)
     email = Column(String(120), unique=True, nullable=False, index=True)
     # Stores the securely hashed password for the user
